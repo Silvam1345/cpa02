dffd7556ebd04dc3c482de2eb6c94e313c9df945
Mateus <Mateus@MacBook-Air.local>
2022-05-04 17:29:42 -0400

    added playerbase tab and updated player model

---
+++ models/Player.js
@@ -0,0 +1,15 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+const Mixed = Schema.Types.Mixed;
+
+var playerSchema = Schema( {
+    firstName: String,
+    lastName: String,
+    playerId: Number,
+    teamId: Number,
+
+});
+
+module.exports = mongoose.model('Player', playerSchema);
